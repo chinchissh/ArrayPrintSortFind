43548b79c565dbc35555788cb0ff00bbd68e7fd5
Sany-SHiriCE <Sany-SHiriCE@402D-9>
2023-09-06 14:30:53 +0700

    Добавьте файлы проекта.

---
+++ Lab2/LeariningTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class LeariningTest
+    {
+        private int[] dataArray;
+
+        public LeariningTest()
+        {
+            Random random = new Random();
+            dataArray = new int[10];
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                dataArray[i] = random.Next(0, 101);
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Массив:");
+            foreach (int number in dataArray)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void SortArray()
+        {
+            Array.Sort(dataArray);
+            Console.WriteLine("Отсортированный массив:");
+            foreach (int number in dataArray)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public int FindElementBinary(int target)
+        {
+            int left = 0;
+            int right = dataArray.Length - 1;
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+
+                if (dataArray[middle] == target)
+                {
+                    return middle;
+                }
+                else if (dataArray[middle] < target)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Lab2;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        LeariningTest test = new LeariningTest();
+
+        test.Print();
+        test.SortArray();
+
+        Console.Write("Найти: ");
+        int target = int.Parse(Console.ReadLine());
+
+        int index = test.FindElementBinary(target);
+
+        if (index != -1)
+        {
+            Console.WriteLine($"Элемент {target} найден на {index}");
+        }
+        else
+        {
+            Console.WriteLine($"Элемент {target} не найден");
+        }
+    }
+}
